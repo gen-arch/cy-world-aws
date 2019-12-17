2de2e766fcc3e9c048e30acb2406cc28bc9fa858
gen <ymdtshm@gmail.com>
2019-12-17 19:33:37 +0900

    init

--- lib/cy-world-aws-stack.ts
+++ lib/cy-world-aws-stack.ts
@@ -1,18 +1,31 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2')
 
 export class CyWorldAwsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CyWorldAwsQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const vpc = new ec2.Vpc(this, 'cy-world-vpc', {
+      cidr: '10.0.0.0/21',
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: 'cy-world-public'
+        },
+        {
+          cidrMask: 24,
+          subnetType: ec2.SubnetType.PRIVATE,
+          name: 'cy-world-private'
+        }
+      ]
     });
 
-    const topic = new sns.Topic(this, 'CyWorldAwsTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const image = new ec2.AmazonLinuxImage();
+    new ec2.Instance(this, 'test-instance', {
+      vpc: vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: image
+    })
   }
 }
