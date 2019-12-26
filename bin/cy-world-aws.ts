83a7f1b6057bf8758a76623e0ee61e346f61e4b9
gen <ymdtshm@gmail.com>
2019-12-27 01:35:24 +0900

    update

--- bin/cy-world-aws.ts
+++ bin/cy-world-aws.ts
@@ -3,4 +3,9 @@ import cdk = require('@aws-cdk/core');
 import { CyWorldAwsStack } from '../lib/cy-world-aws-stack';
 
 const app = new cdk.App();
-new CyWorldAwsStack(app, 'CyWorldAwsStack');
+new CyWorldAwsStack(app, 'CyWorldAwsStack', {
+    env: {
+        region: "ap-northeast-1",
+        account: "681514512350"
+    }
+});
--- lib/cy-world-aws-stack.ts
+++ lib/cy-world-aws-stack.ts
@@ -1,5 +1,8 @@
-import cdk = require('@aws-cdk/core');
-import ec2 = require('@aws-cdk/aws-ec2')
+import cdk      = require('@aws-cdk/core');
+import ec2      = require('@aws-cdk/aws-ec2')
+import r53      = require('@aws-cdk/aws-route53')
+import elbv2    = require('@aws-cdk/aws-elasticloadbalancingv2')
+import targets = require('@aws-cdk/aws-route53-targets')
 
 export class CyWorldAwsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -21,11 +24,56 @@ export class CyWorldAwsStack extends cdk.Stack {
       ]
     });
 
-    const image = new ec2.AmazonLinuxImage();
-    new ec2.Instance(this, 'test-instance', {
-      vpc: vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      machineImage: image
+    const www_sec = new ec2.SecurityGroup(this, 'www', {
+      allowAllOutbound: true,
+      description: 'www sec',
+      vpc: vpc
     })
+
+    www_sec.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80))
+    www_sec.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.icmpPing())
+
+    const image = new ec2.AmazonLinuxImage();
+    const test_ins = new ec2.Instance(this, 'test-instance', {
+      vpc: vpc,
+      vpcSubnets: {
+        subnetName: "cy-world-public"
+      } ,
+      instanceType: new ec2.InstanceType("t2.micro"),
+      machineImage: image,
+      securityGroup: www_sec
+    })
+
+    // const elb = new elbv2.ApplicationLoadBalancer(this, 'LoadBalancer', {
+    //   vpc: vpc,
+    //   loadBalancerName: 'cy-world-public-elb',
+    //   vpcSubnets: {
+    //     subnetName: "cy-world-public"
+    //   } ,
+    //   internetFacing: true
+    // });
+
+    // const tg = new elbv2.ApplicationTargetGroup(this, 'tg', {
+    //   port: 80,
+    //   protocol: elbv2.ApplicationProtocol.HTTP,
+    // })
+
+    // const lisner = elb.addListener('lisner', {
+    //   open: true,
+    //   port: 80,
+    //   protocol: elbv2.ApplicationProtocol.HTTP,
+    //   defaultTargetGroups: [tg]
+    // })
+
+    // tg.addTarget(new elbv2.InstanceTarget(test_ins.instanceId))
+
+    // const zone = r53.HostedZone.fromLookup(this, 'cy-world', {
+    //   domainName: "www.cy-world.com",
+    //   privateZone: false,
+    // })
+    // new r53.ARecord(this, "www.cy-world.com", {
+    //   zone: zone,
+    //   target: r53.RecordTarget.fromIpAddresses("18.182.27.192")
+    // })
   }
 }
