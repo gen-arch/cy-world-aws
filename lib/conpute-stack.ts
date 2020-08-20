67fe49ff6f61506f0cb2573660ff6ff1d592a96e
gen <you@example.com>
2020-08-20 20:25:31 +0900

    remove Unnecessary files

--- lib/conpute-stack.ts
+++
@@ -1,8 +0,0 @@
-import cdk      = require('@aws-cdk/core');
-import ec2      = require('@aws-cdk/aws-ec2')
-
-export class ComputeStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-  }
-}
